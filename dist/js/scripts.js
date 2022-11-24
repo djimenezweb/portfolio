ccd7a5346e19b100285115235cbef43ef0232f14
djimenezchocron <117752348+djimenezchocron@users.noreply.github.com>
2022-11-24 18:27:29 +0100

    Tags proyectos v1

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,15 +1,21 @@
 //Elemento contenedor
-const projectGrid = document.getElementById('project-grid')
-const rootStyles = document.documentElement.style
+// const projectGrid = document.getElementById('project-grid')
+// const rootStyles = document.documentElement.style
 
-projectGrid.addEventListener('mouseover', (event) => {
-    if (event.target.classList.contains('project')) {
-        const imageHeight = event.target.children[0].children[2].clientHeight
-        console.log(imageHeight)
-        rootStyles.setProperty('--transition-velocity', imageHeight / 50 + 's')
-    }
-})
-
+// projectGrid.addEventListener('mouseover', (event) => {
+//     if (event.target.classList.contains('project')) {
+//         const imageHeight = event.target.children[0].children[2].clientHeight
+//         console.log(imageHeight)
+//         rootStyles.setProperty('--transition-velocity', imageHeight / 50 + 's')
+//     }
+// })
 //addEventListener('mouseover',....)
-
 //ev.target.
+
+const formElement = document.getElementById('form')
+const formSubmitElement = document.getElementById('form-submit')
+
+formSubmitElement.addEventListener('click', () => {
+    formElement.submit()
+}
+)
